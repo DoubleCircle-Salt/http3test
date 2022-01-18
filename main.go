e0c44f156a53870975492164649ef6ea9fa760b6
DoubleCircle-Salt <33078105+DoubleCircle-Salt@users.noreply.github.com>
2022-01-18 14:31:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"net/http"
 	"time"
 
 	"github.com/lucas-clemente/quic-go"
@@ -34,7 +35,7 @@ func main() {
 		},
 	}
 
-	request, err := http.NewReqeust("GET", "https://129.227.57.122/index.html", nil)
+	request, err := http.NewRequest("GET", "https://129.227.57.122/index.html", nil)
 	if err != nil {
 		println("create request failed, error:", err.Error())
 		return
