0c73af66a97f48db136f24682ae1044d4e3b8d86
DoubleCircle-Salt <33078105+DoubleCircle-Salt@users.noreply.github.com>
2022-01-18 14:13:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/lucas-clemente/quic-go/http3"
 )
 
+var (
+	port   int
+	server string
+)
+
 func main() {
+
+	flag.IntVar(&port, "p", 443, "server port")
+	flag.StringVar(&server, "s", "", "server address")
 
 }
