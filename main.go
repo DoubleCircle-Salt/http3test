fffa054673a8e8373e689d02441f95efb8fcf550
DoubleCircle-Salt <33078105+DoubleCircle-Salt@users.noreply.github.com>
2022-01-18 15:27:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -25,48 +25,51 @@ func main() {
 	flag.Parse()
 
 
-	roundTripper := &http3.RoundTripper{
-		TLSClientConfig: &tls.Config{
-			ServerName:         "giasstest.ecn.zenlayer.net",
-			ClientSessionCache: tls.NewLRUClientSessionCache(64),
-			InsecureSkipVerify: true,
-		},
-		QuicConfig: &quic.Config{
-			KeepAlive:      true,
-			Versions:       []quic.VersionNumber{quic.VersionDraft29},
-			MaxIdleTimeout: 3 * time.Second,
-		},
-	}
-
 	for i := 0; i < count; i++ {
 		go func() {
-
-			request, err := http.NewRequest("GET", "https://162.221.195.12/test.file", nil)
-			if err != nil {
-				println("create request failed, error:", err.Error())
-				return
-			}
-
-			response, err := roundTripper.RoundTrip(request)
-			if err != nil {
-				println("get response failed, error:", err.Error())
-				return
-			}
-
-			println("status:", response.Status)
-
-			buffer := make([]byte, 4096)
-			n := 0
 			for {
-				nn, err := response.Body.Read(buffer)
-				n += nn
+
+
+				roundTripper := &http3.RoundTripper{
+					TLSClientConfig: &tls.Config{
+						ServerName:         "giasstest.ecn.zenlayer.net",
+						ClientSessionCache: tls.NewLRUClientSessionCache(64),
+						InsecureSkipVerify: true,
+					},
+					QuicConfig: &quic.Config{
+						KeepAlive:      true,
+						Versions:       []quic.VersionNumber{quic.VersionDraft29},
+						MaxIdleTimeout: 3 * time.Second,
+					},
+				}
+
+				request, err := http.NewRequest("GET", "https://162.221.195.12/test.file", nil)
 				if err != nil {
-					println("read failed, err:", err.Error())
+					println("create request failed, error:", err.Error())
 					return
 				}
-			}
 
-			println("read", n, "bytes")
+				response, err := roundTripper.RoundTrip(request)
+				if err != nil {
+					println("get response failed, error:", err.Error())
+					return
+				}
+
+				println("status:", response.Status)
+
+				buffer := make([]byte, 4096)
+				n := 0
+				for {
+					nn, err := response.Body.Read(buffer)
+					n += nn
+					if err != nil {
+						println("read failed, err:", err.Error())
+						return
+					}
+				}
+
+				println("read", n, "bytes")
+			}
 		}()
 	}
 	time.Sleep(time.Hour)
